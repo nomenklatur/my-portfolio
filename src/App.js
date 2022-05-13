03e8ad01f28839d2dc5c64913f03d6850de2e16e
nomenklatur <dimasekaputra@students.usu.ac.id>
2022-05-13 22:00:32 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Testimonials from "./components/Testimonials";
+import Contacts from "./components/Contacts";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contacts />
+    </main>
   );
 }
-
-export default App;
